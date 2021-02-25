3f56bd65886f37cf9ff485e696dfdec45f0be1f7
Marc <nobody@example.com>
2021-02-25 20:47:29 +0100

    renamed some variables

--- hassfeld/auxilliary.py
+++ hassfeld/auxilliary.py
@@ -1,10 +1,11 @@
 """Functions to help dealining with data"""
 
 
-def str_to_bool(s):
-    return s.lower() in ["true", "1", "t", "y", "yes"]
+def str_to_bool(string):
+    """Convert string to bolean"""
+    return string.lower() in ["true", "1", "t", "y", "yes"]
 
 
-def lists_have_same_values(l1, l2):
+def lists_have_same_values(lst1, lst2):
     """Compare two zones disregarding order"""
-    return sorted(l1) == sorted(l2)
+    return sorted(lst1) == sorted(lst2)
