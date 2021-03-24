27b842957ab4cd295e232c6e45721603dc8a668d
Marc <nobody@example.com>
2021-03-24 23:08:18 +0100

    Bump version up to 0.3.6a.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="hassfeld",
-    version="0.3.5-alpha1",
+    version="0.3.6-alpha1",
     description="Integration of Raumfeld into Home Assistant",
     long_description=long_description,
     long_description_content_type="text/x-rst",
